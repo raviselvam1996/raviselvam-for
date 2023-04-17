d4fb6f18ebbc8dd1f296397e41c4ff893240110f
raviselvam1996 <raviselvam1996@gamil.com>
2023-04-17 11:17:28 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'raviselvam';
+
+ 
+
+  movies: Movie[] = [
+    {
+      title: 'Zootopia',
+      director: 'Byron Howard, Rich Moore',
+      cast: 'Idris Elba, Ginnifer Goodwin, Jason Bateman',
+      releaseDate: 'March 4, 2016',
+    },
+    {
+      title: 'Batman v Superman: Dawn of Justice',
+      director: 'Zack Snyder',
+      cast: 'Ben Affleck, Henry Cavill, Amy Adams',
+      releaseDate: 'March 25, 2016',
+    },
+    {
+      title: 'Captain American: Civil War',
+      director: 'Anthony Russo, Joe Russo',
+      cast: 'Scarlett Johansson, Elizabeth Olsen, Chris Evans',
+      releaseDate: 'May 6, 2016',
+    },
+    {
+      title: 'X-Men: Apocalypse',
+      director: 'Bryan Singer',
+      cast: 'Jennifer Lawrence, Olivia Munn, Oscar Isaac',
+      releaseDate: 'May 27, 2016',
+    },
+    {
+      title: 'Warcraft',
+      director: 'Duncan Jones',
+      cast: 'Travis Fimmel, Robert Kazinsky, Ben Foster',
+      releaseDate: 'June 10, 2016',
+    },
+  ];
+}
+
+class Movie {
+  title: string='top movies';
+  director: string='ravi';
+  cast: string='siva';
+  releaseDate: string='23/4/2023';
+}
